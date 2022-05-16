3bfc80bed6db7b0aeffdde90c5e3f615cbdfebcd
prateekgoyal511 <prateekool109@gmail.com>
2022-05-16 08:42:33 +0530

    Find Missing Number in array

---
+++ src/MissingNumber.java
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class MissingNumberMain {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine().trim());
+        String[] str = br.readLine().trim().split(" ");
+        int[] array = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            array[i] = Integer.parseInt(str[i]);
+        }
+        MissingNumber sln = new MissingNumber();
+        System.out.println(sln.MissingNumber(array, n));
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class MissingNumber {
+    int MissingNumber(int array[], int n) {
+        // Your Code Here
+        int expectedSum = calculateExpectedSum(n);
+        int actualSum = calculateActualSum(array, n);
+
+        return (expectedSum - actualSum);
+    }
+
+    int calculateExpectedSum(int n) {
+        if(n%2 == 0) {
+            return (n/2)*(n+1);
+        } else {
+            return ((n+1)/2)*n;
+        }
+    }
+
+    int calculateActualSum(int array[], int n) {
+        int i;
+
+        int sum = 0;
+
+        for(i=0; i<n-1; i++) {
+            sum = sum + array[i];
+        }
+        return sum;
+    }
+}
